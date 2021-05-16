96758a8776ede7df63bd07e2cec52516dbc8dc94
Markus <markus.h.jarvis@gmail.com>
2021-05-16 17:49:37 -0600

    Update is_Unique.js

--- arrays and strings/is_Unique.js
+++ arrays and strings/is_Unique.js
@@ -1,3 +1,8 @@
+
+/* 
+ * Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+ */
+
 /* Array to hold 26 values coresponding to the alphabet*/
 let arr = [];
 
