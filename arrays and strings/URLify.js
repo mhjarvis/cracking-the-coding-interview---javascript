c60f28b48d97999d946f952be27b1d90da6c80bd
Markus <markus.h.jarvis@outlook.com>
2021-05-19 16:56:05 -0600

    Add URLify function

---
+++ arrays and strings/URLify.js
@@ -0,0 +1,32 @@
+/* Write a method to replace all spaces in a string with '%20'. Assume that the string
+ * has sufficient space at the end to hold the additional characters.
+ */
+
+let t0 = performance.now();
+console.log(URLify("Mr John Smith   "));
+let t1 = performance.now();
+
+let t2 = performance.now();
+console.log(simpleURLify("Mr John Smith   "));
+let t3 = performance.now();
+
+console.log("URLify function: " + (t1 - t0));
+console.log("simpleURLIFY function: " + (t3 - t2))
+
+/* Function to URLify string using basic for loop */
+function URLify(str) {
+
+    str = str.trim().split("");
+    
+    for(let i = 0; i < length; i++) {
+        if(str[i] == ' ') {
+            str[i] = '%20';
+        }
+    }
+    return str = str.join("");
+}
+
+/* Simpler version of urlify function */
+function simpleURLify(str) {
+    return str.trim().replace(/\s/g, '%20');
+}
